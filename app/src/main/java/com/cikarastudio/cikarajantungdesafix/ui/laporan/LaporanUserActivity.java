c6bcc08b649c7ced5f38170cbf78f4a5f99419de
RestutriewLaptop <restutriew@gmail.com>
2021-11-04 17:20:09 +0700

    update tampilan sampai laporan

--- app/src/main/java/com/cikarastudio/cikarajantungdesafix/ui/laporan/LaporanUserActivity.java
+++ app/src/main/java/com/cikarastudio/cikarajantungdesafix/ui/laporan/LaporanUserActivity.java
@@ -3,15 +3,27 @@ package com.cikarastudio.cikarajantungdesafix.ui.laporan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.cikarastudio.cikarajantungdesafix.R;
 
 public class LaporanUserActivity extends AppCompatActivity {
 
+    ImageView img_back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_laporan_user);
+
+        img_back = findViewById(R.id.img_back);
+        img_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
     }
 }
